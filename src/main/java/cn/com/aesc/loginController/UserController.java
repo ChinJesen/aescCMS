5964090d992f2f5b45129ce659c4352601563904
ChinJesen <1036359208@qq.com>
2018-05-22 17:22:57 +0800

    主页问题已经解决。用户复选框的选择问题待处理
    主页是因为多了一个div导致的排版不正确

---
+++ src/main/java/cn/com/aesc/loginController/UserController.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2005-2018, 重庆汽博实业有限公司
+ * FileName: UserController
+ * Author:   dawn@acdiost.com
+ * Date:     2018-05-22 12:34
+ * Description: 用户管理类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package cn.com.aesc.loginController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈用户管理类〉
+ *
+ * @author dawn@acdiost.com
+ * @create 2018-05-22
+ * @since 1.0.0
+ */
+@Controller
+public class UserController {
+  @RequestMapping("/inquiryAllUsers")
+  String inquiry(){
+    return "userHtml/user";
+  }
+}

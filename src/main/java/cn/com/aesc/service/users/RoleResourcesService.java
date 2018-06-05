a432983e728f62c0684a39d3470ca373b1792c71
ChinJesen <1036359208@qq.com>
2018-06-05 17:45:23 +0800

    恢复到未添加shiro时

--- src/main/java/cn/com/aesc/service/users/RoleResourcesService.java
+++
@@ -1,8 +0,0 @@
-package cn.com.aesc.service.users;
-
-import cn.com.aesc.pojo.users.RoleResources;
-import cn.com.aesc.service.IService;
-
-public interface RoleResourcesService extends IService<RoleResources> {
-  void addRoleResources(RoleResources roleResources);
-}
--- src/main/java/cn/com/aesc/utils/MyMapper.java
+++ src/main/java/cn/com/aesc/utils/MyMapper.java
@@ -3,5 +3,17 @@ package cn.com.aesc.utils;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+/**
+ * Copyright (C), 2005-2018, 重庆汽博实业有限公司
+ * 
+ * @Author: dawn@acdiost.com
+ * @Date: 2018-06-05 14:58
+ * @Param: 
+ * @Return: 
+ * @See: 
+ * @Throws: 
+ * @Version: 
+ * @Description: 
+ */
 public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
 }
